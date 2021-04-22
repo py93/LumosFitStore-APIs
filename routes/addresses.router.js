8b224c75457686c54fc9fa31a36b8101826b3016
Pooja Sabnani <>
2021-04-22 02:20:48 +0000

    feat: post API for address

--- routes/addresses.router.js
+++ routes/addresses.router.js
@@ -12,5 +12,16 @@ addressRouter.route('/')
     res.status(500).json({success:false, message: "Unable to get addresses", errorMessage: err.message})
   }
 })
-
+.post( async (req,res)=>{
+  try{
+    const address = req.body;
+    const newAddress = new Address(product);
+    const savedAddress = await newAddress.save();
+    res.json({success: true, address: savedAddress})
+  }
+  catch(err)
+  {
+    res.status(500).json({success:false, message: "Unable to add product", errorMessage: err.message})
+  }
+}) 
 module.exports = addressRouter
