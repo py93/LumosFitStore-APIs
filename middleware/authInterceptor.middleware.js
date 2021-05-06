31d829f2716b2cb2e3e94bd89e8e2a6dcef46501
py93 <47155755+py93@users.noreply.github.com>
2021-05-06 20:23:53 +0530

    reverting commit

--- middleware/authInterceptor.middleware.js
+++
@@ -1,6 +0,0 @@
-function authInterceptor (req,res){
-  //TODO: Check for usename pwd auth
-  next();
-}
-
-module.exports = {authInterceptor}
--- models/category.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-const CategorySchema = new mongoose.Schema({name: String, description: String})
-
-const Category = mongoose.model("Category", CategorySchema)
-module.exports = {Category}
