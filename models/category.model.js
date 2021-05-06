d350c06a2640f809b6e6580d297f1d5bcb16636f
py93 <47155755+py93@users.noreply.github.com>
2021-05-06 21:48:40 +0530

    feat: database, middleware, apis added

---
+++ models/category.model.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+
+const CategorySchema = new mongoose.Schema({name: String, description: String})
+
+const Category = mongoose.model("Category", CategorySchema)
+module.exports = {Category}
