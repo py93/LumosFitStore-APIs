4ae373864df68354b4e7600d3aa7fb3cbcc85fa1
Pooja Sabnani <>
2021-05-06 17:03:35 +0000

    feat: added validations

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
 
-const UserSchema = new mongoose.Schema({userId: String, password: String})
-
+const UserSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: "This is mandatory"
+  },
+  password:{
+    type: String,
+    required: "Password cannot be blank"
+  }
+})
 const User = mongoose.model("User", UserSchema)
 module.exports = {User}
