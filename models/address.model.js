d89d30cea911d0d6c5e776cf050e6e4d69c55ea8
py93 <47155755+py93@users.noreply.github.com>
2021-05-06 21:46:57 +0530

    reverting commits

--- models/address.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const AddressSchema = new mongoose.Schema({fullName: String, addressLine1: String, addressLine2: String, city: String, state: String, country: String, zipCode: String, contactNo: String});
-
-const Address = mongoose.model("Address", AddressSchema)
-
-module.exports = {Address, AddressSchema}
